cf90bb777e1e6746064eadcd78bc7ee020f38506
Stafford Horne <shorne@gmail.com>
2016-11-15 23:13:49 +0900

    or1k: sim: add additional stubs for linux build
    Building with musl 1.1.14 the traps interface requires to be
    expanded more.  For now just add stubs to get building. Also
    corelow is no longer needed in configure.tgt as its already
    included.

--- sim/or1k/traps-linux.c
+++ sim/or1k/traps-linux.c
@@ -6,3 +6,24 @@ sim_engine_invalid_insn (SIM_CPU *current_cpu, IADDR cia, SEM_PC vpc)
   /* TODO */
   abort();
 }
+
+void or1k32bf_exception (sim_cpu *current_cpu, USI pc, USI exnum)
+{
+  /* TODO */
+  abort();
+}
+
+void or1k32bf_rfe (sim_cpu *current_cpu)
+{
+  abort();
+}
+
+USI or1k32bf_mfspr (sim_cpu *current_cpu, USI addr)
+{
+  abort();
+}
+
+void or1k32bf_mtspr (sim_cpu *current_cpu, USI addr, USI val)
+{
+  abort();
+}
