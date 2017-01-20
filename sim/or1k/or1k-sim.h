92c04bd768b66a48e03850af185e975d01605435
Stafford Horne <shorne@gmail.com>
2017-01-21 00:45:13 +0900

    sim: or1k: add or1k target to sim
    sim/ChangeLog:
    
    2017-01-20  Stafford Horne  <shorne@gmail.com>
    	    Peter Gavin  <pgavin@gmail.com>
    
    	* configure.tgt: Add or1k sim.
    	* or1k/Makefile.in: New file.
    	* or1k/arch.h: New file.
    	* or1k/configure.ac: New file.
    	* or1k/cpu.h: New file.
    	* or1k/cpuall.h: New file.
    	* or1k/decode.h: New file.
    	* or1k/eng.h: New file.
    	* or1k/mloop.in: New file.
    	* or1k/or1k-sim.h: New file.
    	* or1k/or1k.c: New file.
    	* or1k/or1k.h: New file.
    	* or1k/sim-if.c: New file.
    	* or1k/sim-main.h: New file.
    	* or1k/traps-linux.c: New file.
    	* or1k/traps.c: New file.

---
+++ sim/or1k/or1k-sim.h
@@ -0,0 +1,21 @@
+#ifndef OR1K_SIM_H
+#define OR1K_SIM_H
+
+#include "symcat.h"
+
+/* GDB register numbers.  */
+#define PPC_REGNUM	32
+#define PC_REGNUM	33
+#define SR_REGNUM	34
+
+/* Misc. profile data.  */
+typedef struct {
+} OR1K_MISC_PROFILE;
+
+#define GETTWI XCONCAT2(GETT,WI)
+#define SETTWI XCONCAT2(SETT,WI)
+
+#define EXIT_ERROR       1
+#define EXIT_UNSPECIFIED 2
+
+#endif /* OR1K_SIM_H */
---
+++ sim/or1k/traps-linux.c
@@ -0,0 +1,32 @@
+#include "sim-main.h"
+
+SEM_PC
+sim_engine_invalid_insn (SIM_CPU *current_cpu, IADDR cia, SEM_PC vpc)
+{
+  /* TODO */
+  abort();
+}
+
+void or1k32bf_exception (sim_cpu *current_cpu, USI pc, USI exnum)
+{
+  /* TODO */
+  abort();
+}
+
+void or1k32bf_rfe (sim_cpu *current_cpu)
+{
+  /* TODO */
+  abort();
+}
+
+USI or1k32bf_mfspr (sim_cpu *current_cpu, USI addr)
+{
+  /* TODO */
+  abort();
+}
+
+void or1k32bf_mtspr (sim_cpu *current_cpu, USI addr, USI val)
+{
+  /* TODO */
+  abort();
+}
