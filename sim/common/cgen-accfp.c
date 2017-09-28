f21e8fea7d3aa47bee597116e501d874fe52a5f2
Stafford Horne <shorne@gmail.com>
2017-09-29 00:29:03 +0900

    sim: common: Fix pointer sign warnings
    When compiling we get the following warnings:
    
      common/cgen-accfp.c: In function ‘fixsfsi’:
      common/cgen-accfp.c:370:18: warning: pointer targets in passing argument 1 of ‘sim_fpu_to32i’ differ in signedness [-Wpointer-sign]
         sim_fpu_to32i (&res, &op1, sim_fpu_round_near);
                      ^
      common/cgen-accfp.c: In function ‘fixdfsi’:
      common/cgen-accfp.c:381:18: warning: pointer targets in passing argument 1 of ‘sim_fpu_to32i’ differ in signedness [-Wpointer-sign]
         sim_fpu_to32i (&res, &op1, sim_fpu_round_near);
                      ^
    sim/common/ChangeLog:
    
    yyyy-mm-dd  Stafford Horne  <shorne@gmail.com>
    
    	* cgen-accfp.c (fixsfsi): Change res from unsigned32 to signed32.
    	(fixdfsi): Change res from unsigned32 to signed32.

--- sim/common/cgen-accfp.c
+++ sim/common/cgen-accfp.c
@@ -364,7 +364,7 @@ static SI
 fixsfsi (CGEN_FPU* fpu, int how UNUSED, SF x)
 {
   sim_fpu op1;
-  unsigned32 res;
+  signed32 res;
 
   sim_fpu_32to (&op1, x);
   sim_fpu_to32i (&res, &op1, sim_fpu_round_near);
@@ -375,7 +375,7 @@ static SI
 fixdfsi (CGEN_FPU* fpu, int how UNUSED, DF x)
 {
   sim_fpu op1;
-  unsigned32 res;
+  signed32 res;
 
   sim_fpu_64to (&op1, x);
   sim_fpu_to32i (&res, &op1, sim_fpu_round_near);
